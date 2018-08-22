6eda4bd5c787b3341683af30b8a8d558b72f5160
Gilbert Samuelian <gsamuelian@blizzard.com>
2018-08-21 19:30:35 -0700

    Removing some tests for public version

--- CultureIcuTest/CultureIcuTest.h
+++ CultureIcuTest/CultureIcuTest.h
@@ -3,5 +3,3 @@
 void outputZhLocales(const std::vector<std::string>& all_locale_names);
 void getAvailableCppApiTest();
 void getAvailableCApiTest();
-void getNameCApiTest();
-void getLocaleCoreClrStyleTest();
